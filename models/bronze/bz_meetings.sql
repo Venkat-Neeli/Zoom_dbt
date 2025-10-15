b697722b2e58fac5c9a3a312aead0e79985df5b1
Venkat-Neeli <venkateshneeli2001@gmail.com>
2025-10-15 21:33:02 +0530

    Automated DBT Sync

--- models/bronze/bz_meetings.sql
+++ models/bronze/bz_meetings.sql
@@ -17,7 +17,7 @@ WITH source_data AS (
         load_timestamp,
         update_timestamp,
         source_system
-    FROM {{ source('raw_data', 'meetings') }}
+    FROM {{ source('RAW', 'meetings') }}
     WHERE meeting_id IS NOT NULL -- Basic data quality check
 ),
 
