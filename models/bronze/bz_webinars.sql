34e51ab81ea9695ce580de82964e573eb4593106
Venkat-Neeli <venkateshneeli2001@gmail.com>
2025-10-15 21:29:58 +0530

    Automated DBT Sync

---
+++ models/bronze/bz_webinars.sql
@@ -0,0 +1,38 @@
+{{
+  config(
+    materialized='table',
+    pre_hook="INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status) SELECT 'bz_webinars', CURRENT_TIMESTAMP(), 'DBT_SYSTEM', 0, 'STARTED' WHERE '{{ this.name }}' != 'bz_audit_log'",
+    post_hook="INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status) SELECT 'bz_webinars', CURRENT_TIMESTAMP(), 'DBT_SYSTEM', DATEDIFF('second', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'bz_webinars' AND status = 'STARTED'), CURRENT_TIMESTAMP()), 'COMPLETED' WHERE '{{ this.name }}' != 'bz_audit_log'"
+  )
+}}
+
+WITH source_data AS (
+    SELECT 
+        webinar_id,
+        host_id,
+        webinar_topic,
+        start_time,
+        end_time,
+        registrants,
+        load_timestamp,
+        update_timestamp,
+        source_system
+    FROM {{ source('raw_data', 'webinars') }}
+    WHERE webinar_id IS NOT NULL -- Basic data quality check
+),
+
+final AS (
+    SELECT 
+        webinar_id,
+        host_id,
+        webinar_topic,
+        start_time,
+        end_time,
+        registrants,
+        COALESCE(load_timestamp, CURRENT_TIMESTAMP()) as load_timestamp,
+        COALESCE(update_timestamp, CURRENT_TIMESTAMP()) as update_timestamp,
+        COALESCE(source_system, 'ZOOM_PLATFORM') as source_system
+    FROM source_data
+)
+
+SELECT * FROM final
