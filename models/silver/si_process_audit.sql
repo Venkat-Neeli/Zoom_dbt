a58f088cd2e90fcd633de7e6ec7dc100e069a0ef
Venkat-Neeli <venkateshneeli2001@gmail.com>
2025-10-16 17:02:25 +0530

    Automated DBT Sync

--- models/silver/si_process_audit.sql
+++ models/silver/si_process_audit.sql
@@ -1,23 +1,24 @@
 {{ config(
     materialized='incremental',
     unique_key='execution_id',
-    on_schema_change='fail'
+    on_schema_change='sync_all_columns',
+    tags=['audit', 'silver']
 ) }}
 
 WITH audit_data AS (
     SELECT
-        {{ dbt_utils.generate_surrogate_key(['current_timestamp()']) }} AS execution_id,
-        'bronze_to_silver_transformation' AS pipeline_name,
+        {{ dbt_utils.generate_surrogate_key(['current_timestamp()', 'invocation_id']) }} AS execution_id,
+        'Bronze_to_Silver_Transform' AS pipeline_name,
         CURRENT_TIMESTAMP() AS start_time,
         CURRENT_TIMESTAMP() AS end_time,
-        'SUCCESS' AS status,
+        'RUNNING' AS status,
         NULL AS error_message,
         0 AS records_processed,
         0 AS records_successful,
         0 AS records_failed,
         0 AS processing_duration_seconds,
-        'Zoom' AS source_system,
-        'Silver' AS target_system,
+        'DBT' AS source_system,
+        'SILVER' AS target_system,
         'ETL' AS process_type,
         'dbt_user' AS user_executed,
         'dbt_cloud' AS server_name,
@@ -27,27 +28,7 @@ WITH audit_data AS (
         CURRENT_DATE() AS update_date
 )
 
-SELECT 
-    execution_id,
-    pipeline_name,
-    start_time,
-    end_time,
-    status,
-    error_message,
-    records_processed,
-    records_successful,
-    records_failed,
-    processing_duration_seconds,
-    source_system,
-    target_system,
-    process_type,
-    user_executed,
-    server_name,
-    memory_usage_mb,
-    cpu_usage_percent,
-    load_date,
-    update_date
-FROM audit_data
+SELECT * FROM audit_data
 
 {% if is_incremental() %}
     WHERE start_time > (SELECT MAX(start_time) FROM {{ this }})
