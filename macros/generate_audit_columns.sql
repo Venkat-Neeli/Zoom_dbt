cc664597a928ce63a5b6824447207bcb231db108
Venkat-Neeli <venkateshneeli2001@gmail.com>
2025-10-17 21:08:48 +0530

    Automated DBT Sync

---
+++ macros/generate_audit_columns.sql
@@ -0,0 +1,7 @@
+{% macro generate_audit_columns() %}
+  CURRENT_TIMESTAMP() as load_timestamp,
+  CURRENT_TIMESTAMP() as update_timestamp,
+  CURRENT_DATE() as load_date,
+  CURRENT_DATE() as update_date,
+  'DBT_PIPELINE' as source_system
+{% endmacro %}
