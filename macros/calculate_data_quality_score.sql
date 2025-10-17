e3668c4a32fcfe719d61a9bb6f9438693fd0f07b
Venkat-Neeli <venkateshneeli2001@gmail.com>
2025-10-17 22:11:43 +0530

    Delete macros directory

--- macros/calculate_data_quality_score.sql
+++
@@ -1,9 +0,0 @@
-{% macro calculate_data_quality_score(null_checks, format_checks, range_checks) %}
-    CASE 
-        WHEN ({{ null_checks }}) AND ({{ format_checks }}) AND ({{ range_checks }}) THEN 1.00
-        WHEN ({{ null_checks }}) AND ({{ format_checks }}) THEN 0.80
-        WHEN ({{ null_checks }}) AND ({{ range_checks }}) THEN 0.70
-        WHEN ({{ null_checks }}) THEN 0.60
-        ELSE 0.00
-    END
-{% endmacro %}
