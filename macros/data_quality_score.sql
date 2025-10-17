ffb27b2d3499e7a77e1c69d823b208c1af3e9c1f
Venkat-Neeli <venkateshneeli2001@gmail.com>
2025-10-17 21:08:47 +0530

    Automated DBT Sync

--- macros/data_quality_score.sql
+++ macros/data_quality_score.sql
@@ -1,7 +1,7 @@
 {% macro calculate_data_quality_score(table_name, columns) %}
   CASE 
-    {% for column in columns %}
-    WHEN {{ column }} IS NULL THEN 0.0
+    {% for col in columns %}
+    WHEN {{ col }} IS NULL THEN 0.0
     {% endfor %}
     ELSE 1.0
   END
