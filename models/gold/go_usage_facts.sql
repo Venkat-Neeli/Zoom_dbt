1cc29a9a920321ea6133d3984962973b2ca96c2f
Venkat-Neeli <venkateshneeli2001@gmail.com>
2025-10-20 18:51:11 +0530

    Automated DBT Sync

--- models/gold/go_usage_facts.sql
+++ models/gold/go_usage_facts.sql
@@ -1,7 +1,4 @@
-{{ config(
-    materialized='table',
-    schema='gold'
-) }}
+{{ config(materialized='table') }}
 
 SELECT 
     usage_id,
@@ -9,12 +6,6 @@ SELECT
     feature_name,
     usage_count,
     usage_date,
-    load_date,
-    update_date,
-    source_system,
-    data_quality_score,
-    HASH(usage_id || meeting_id) as usage_key,
     CURRENT_TIMESTAMP() as created_at
 FROM {{ ref('si_feature_usage') }}
 WHERE record_status = 'ACTIVE'
-    AND data_quality_score >= 0.7
