26f0c5eb0941cea281e2949e38ac4594f4788a28
Venkat-Neeli <venkateshneeli2001@gmail.com>
2025-10-20 18:51:08 +0530

    Automated DBT Sync

--- models/gold/go_meeting_facts.sql
+++ models/gold/go_meeting_facts.sql
@@ -1,7 +1,4 @@
-{{ config(
-    materialized='table',
-    schema='gold'
-) }}
+{{ config(materialized='table') }}
 
 SELECT 
     meeting_id,
@@ -10,12 +7,6 @@ SELECT
     start_time,
     end_time,
     duration_minutes,
-    load_date,
-    update_date,
-    source_system,
-    data_quality_score,
-    HASH(meeting_id || host_id) as meeting_key,
     CURRENT_TIMESTAMP() as created_at
 FROM {{ ref('si_meetings') }}
 WHERE record_status = 'ACTIVE'
-    AND data_quality_score >= 0.7
