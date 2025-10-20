1608a1b316f24c4bf36e1eb4d64a7f8f65f0aa9f
Venkat-Neeli <venkateshneeli2001@gmail.com>
2025-10-20 18:51:10 +0530

    Automated DBT Sync

--- models/gold/go_billing_facts.sql
+++ models/gold/go_billing_facts.sql
@@ -1,7 +1,4 @@
-{{ config(
-    materialized='table',
-    schema='gold'
-) }}
+{{ config(materialized='table') }}
 
 SELECT 
     event_id,
@@ -9,12 +6,6 @@ SELECT
     event_type,
     amount,
     event_date,
-    load_date,
-    update_date,
-    source_system,
-    data_quality_score,
-    HASH(event_id || user_id) as billing_key,
     CURRENT_TIMESTAMP() as created_at
 FROM {{ ref('si_billing_events') }}
 WHERE record_status = 'ACTIVE'
-    AND data_quality_score >= 0.7
