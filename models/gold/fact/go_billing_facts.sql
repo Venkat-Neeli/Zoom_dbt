b962e2a423318cfbfdab4535ed40a0c79d42b5fa
Venkat-Neeli <venkateshneeli2001@gmail.com>
2025-10-21 11:51:38 +0530

    Automated DBT Sync

--- models/gold/fact/go_billing_facts.sql
+++ models/gold/fact/go_billing_facts.sql
@@ -1,23 +1,19 @@
-{{ config(
-    materialized='table'
-) }}
-
 SELECT 
-    'BF_SAMPLE_001' AS billing_fact_id,
+    'BF_001' AS billing_fact_id,
     'EVENT_001' AS event_id,
     'USER_001' AS user_id,
     'ORG_001' AS organization_id,
     'SUBSCRIPTION' AS event_type,
     29.99 AS amount,
-    CURRENT_DATE() AS event_date,
-    DATE_TRUNC('month', CURRENT_DATE()) AS billing_period_start,
-    LAST_DAY(CURRENT_DATE()) AS billing_period_end,
+    '2024-01-01'::DATE AS event_date,
+    '2024-01-01'::DATE AS billing_period_start,
+    '2024-01-31'::DATE AS billing_period_end,
     'Credit Card' AS payment_method,
     'Completed' AS transaction_status,
     'USD' AS currency_code,
     2.40 AS tax_amount,
     0.00 AS discount_amount,
-    CURRENT_DATE() AS load_date,
-    CURRENT_DATE() AS update_date,
+    '2024-01-01'::DATE AS load_date,
+    '2024-01-01'::DATE AS update_date,
     'ZOOM_API' AS source_system
 WHERE 1=0
