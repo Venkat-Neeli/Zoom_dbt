f3a0de9fefdbda4fbafbcc0cfdb11c74443fa05b
Venkat-Neeli <venkateshneeli2001@gmail.com>
2025-10-21 11:51:38 +0530

    Automated DBT Sync

--- models/gold/fact/go_webinar_facts.sql
+++ models/gold/fact/go_webinar_facts.sql
@@ -1,14 +1,10 @@
-{{ config(
-    materialized='table'
-) }}
-
 SELECT 
-    'WF_SAMPLE_001' AS webinar_fact_id,
+    'WF_001' AS webinar_fact_id,
     'WEBINAR_001' AS webinar_id,
     'HOST_001' AS host_id,
-    'Sample Webinar Topic' AS webinar_topic,
-    CURRENT_TIMESTAMP() AS start_time,
-    DATEADD('minute', 90, CURRENT_TIMESTAMP()) AS end_time,
+    'Test Webinar' AS webinar_topic,
+    '2024-01-01 14:00:00'::TIMESTAMP AS start_time,
+    '2024-01-01 15:30:00'::TIMESTAMP AS end_time,
     90 AS duration_minutes,
     100 AS registrants_count,
     75 AS actual_attendees,
@@ -18,7 +14,7 @@ SELECT
     8 AS poll_responses_count,
     7.8 AS engagement_score,
     'Standard' AS event_category,
-    CURRENT_DATE() AS load_date,
-    CURRENT_DATE() AS update_date,
+    '2024-01-01'::DATE AS load_date,
+    '2024-01-01'::DATE AS update_date,
     'ZOOM_API' AS source_system
 WHERE 1=0
