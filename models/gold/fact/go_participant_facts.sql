0ae48af7cd85848902c05ac4bbd48f2db12e4efb
Venkat-Neeli <venkateshneeli2001@gmail.com>
2025-10-21 11:51:37 +0530

    Automated DBT Sync

--- models/gold/fact/go_participant_facts.sql
+++ models/gold/fact/go_participant_facts.sql
@@ -1,14 +1,10 @@
-{{ config(
-    materialized='table'
-) }}
-
 SELECT 
-    'PF_SAMPLE_001' AS participant_fact_id,
-    'SAMPLE_MEETING_001' AS meeting_id,
+    'PF_001' AS participant_fact_id,
+    'MEETING_001' AS meeting_id,
     'PARTICIPANT_001' AS participant_id,
     'USER_001' AS user_id,
-    CURRENT_TIMESTAMP() AS join_time,
-    DATEADD('minute', 45, CURRENT_TIMESTAMP()) AS leave_time,
+    '2024-01-01 10:00:00'::TIMESTAMP AS join_time,
+    '2024-01-01 10:45:00'::TIMESTAMP AS leave_time,
     45 AS attendance_duration,
     'Participant' AS participant_role,
     'Computer Audio' AS audio_connection_type,
@@ -19,7 +15,7 @@ SELECT
     8.2 AS connection_quality_rating,
     'Desktop' AS device_type,
     'US-East' AS geographic_location,
-    CURRENT_DATE() AS load_date,
-    CURRENT_DATE() AS update_date,
+    '2024-01-01'::DATE AS load_date,
+    '2024-01-01'::DATE AS update_date,
     'ZOOM_API' AS source_system
 WHERE 1=0
