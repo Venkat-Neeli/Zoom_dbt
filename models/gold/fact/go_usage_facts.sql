75ce145998683c16517ae05600cd6464de584bda
Venkat-Neeli <venkateshneeli2001@gmail.com>
2025-10-21 11:51:39 +0530

    Automated DBT Sync

--- models/gold/fact/go_usage_facts.sql
+++ models/gold/fact/go_usage_facts.sql
@@ -1,12 +1,8 @@
-{{ config(
-    materialized='table'
-) }}
-
 SELECT 
-    'UF_SAMPLE_001' AS usage_fact_id,
+    'UF_001' AS usage_fact_id,
     'USER_001' AS user_id,
     'ORG_001' AS organization_id,
-    CURRENT_DATE() AS usage_date,
+    '2024-01-01'::DATE AS usage_date,
     3 AS meeting_count,
     180 AS total_meeting_minutes,
     1 AS webinar_count,
@@ -14,7 +10,7 @@ SELECT
     2.5 AS recording_storage_gb,
     25 AS feature_usage_count,
     15 AS unique_participants_hosted,
-    CURRENT_DATE() AS load_date,
-    CURRENT_DATE() AS update_date,
+    '2024-01-01'::DATE AS load_date,
+    '2024-01-01'::DATE AS update_date,
     'ZOOM_API' AS source_system
 WHERE 1=0
